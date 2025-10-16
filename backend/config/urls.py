c08a136c6bf143108d2d65dd356e34811de9d0e0
Codenburg <tomasoldenburg@hotmail.com>
2025-10-15 21:08:00 -0300

    Refactor project structure: update .gitignore, add Docker support, and enhance environment configuration

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from backend.config.views import test_origin
+from config.views import test_origin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
