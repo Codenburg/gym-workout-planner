26c98456d6411f18f8fe32b1eff99126d5347523
Codenburg <tomasoldenburg@hotmail.com>
2025-10-17 11:29:55 -0300

    Fix environment variable typo and switch to env_file configuration for Postgres DB

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": env.str("POSTGRES_DB"),
         "USER": env.str("POSTGRES_USER"),
-        "PASSWORD": env.str("POSTRGRES_PASSWORD"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD"),
         "HOST": env.str("POSTGRES_HOST"),
         "PORT": env.str("POSTGRES_PORT"),
     }
