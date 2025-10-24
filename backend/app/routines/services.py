d12a599f39b2ca5209ce4879d0b35f20779c3f30
Codenburg <tomasoldenburg@hotmail.com>
2025-10-24 13:51:40 -0300

    feat: Add routines app and QR code generation functionality

---
+++ backend/app/routines/services.py
@@ -0,0 +1,9 @@
+import qrcode
+from io import BytesIO
+from django.core.files import File
+
+def generate_qr(routine):
+    qr_image = qrcode.make(f"https://tugym.com/routine/{routine.id}")
+    buffer = BytesIO()
+    qr_image.save(buffer, format="PNG")
+    routine.qr_code.save(f"qr_{routine.id}.png", File(buffer), save=False)
